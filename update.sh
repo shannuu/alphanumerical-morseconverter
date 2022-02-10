43ae753f697b814537ca4db34d52acfe8eabc9bc
Kushal <64453862+shannuu@users.noreply.github.com>
2022-02-10 22:23:46 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,21 @@
+repnamer(){
+cd
+list=$(ls)
+count=$(echo $list | wc -w)
+rcount=$(( $count + 1 ))
+var=1
+until [[ $var == $rcount ]]; do
+rep=$(echo $list | cut -d " " -f $var)
+frep=$(echo $rep | tr '[:lower:]' '[:upper:]')
+if [[ $frep == "ALPHANUMERICAL-MORSECONVERTER" ]]; then
+break
+exit
+fi
+((var++))
+done
+}
+repnamer
+rm -rf $rep > /dev/null 2>&1
+pkg install git -y > /dev/null 2>&1
+git clone https://github.com/Shannuu/AlphaNumerical-MorseConverter > /dev/null 2>&1
+echo "Done"
